66e789f4b5d178daaa4b48cf0f78a8e4139fe49d
Tyler Long <tyler4long@gmail.com>
2020-06-09 17:30:55 -0700

    JSON.net

--- EngageVoiceDemo/Program.cs
+++ EngageVoiceDemo/Program.cs
@@ -3,17 +3,26 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using dotenv.net;
 using RingCentral;
+using Newtonsoft.Json;
 
 namespace EngageVoiceDemo
 {
+    class EngageVoiceToken
+    {
+        public string refreshToken;
+        public string accessToken;
+        public string tokenType;
+        public string platformId;
+    }
     
     class Program
     {
         public static HttpClient httpClient = new HttpClient();
+        public static string engageVoiceServer = Environment.GetEnvironmentVariable("ENGAGE_VOICE_SERVER_URL");
         
         static void Main(string[] args)
         {
-            DotEnv.Config(false);
+            DotEnv.Config(true);
             
             Task.Run(async () =>
             {
@@ -28,18 +37,28 @@ namespace EngageVoiceDemo
                         Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
                         Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
                     );
-                    Console.WriteLine(rc.token.access_token);
                     
                     var dict = new System.Collections.Generic.Dictionary<string, string>();
                     dict.Add("rcAccessToken", rc.token.access_token);
                     dict.Add("rcTokenType", "Bearer");
+                    var uriBuilder = new UriBuilder($"{engageVoiceServer}/api/auth/login/rc/accesstoken");
+                    // uriBuilder.Query = $"rcAccessToken={rc.token.access_token}&rcTokenType=Bearer";
                     var httpRequestMessage = new HttpRequestMessage
                     {
                         Method = HttpMethod.Post,
-                        RequestUri = new Uri("https://engage.ringcentral.com/api/auth/login/rc/accesstoken"),
+                        RequestUri = uriBuilder.Uri,
                         Content = new FormUrlEncodedContent(dict)
                     };
                     var r = await httpClient.SendAsync(httpRequestMessage);
+                    var engageVoiceToken =
+                        JsonConvert.DeserializeObject<EngageVoiceToken>(await r.Content.ReadAsStringAsync());
+
+                    r = await httpClient.SendAsync(new HttpRequestMessage
+                    {
+                        Method = HttpMethod.Get,
+                        Headers = { {"Authorization", $"Bearer {engageVoiceToken.accessToken}"} },
+                        RequestUri = new Uri($"{engageVoiceServer}/voice/api/v1/admin/accounts")
+                    });
                     Console.WriteLine(await r.Content.ReadAsStringAsync());
                     
                     await rc.Revoke();
